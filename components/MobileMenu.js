ffb81212275d2f9ede3d73ad0c472d8082dc55f4
Ram Murmu <runashInc@outlook.com>
2022-05-16 12:00:06 +0530

    Update MobileMenu.js

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -61,7 +61,7 @@ export default function MobileMenu() {
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
             style={{ transitionDelay: '175ms' }}
           >
-            <Link href="/guestbook">
+            <Link href="/">
               <a className="flex w-auto pb-4">Guestbook</a>
             </Link>
           </li>
@@ -69,7 +69,7 @@ export default function MobileMenu() {
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
             style={{ transitionDelay: '200ms' }}
           >
-            <Link href="/dashboard">
+            <Link href="/">
               <a className="flex w-auto pb-4">Dashboard</a>
             </Link>
           </li>
@@ -77,7 +77,7 @@ export default function MobileMenu() {
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
             style={{ transitionDelay: '250ms' }}
           >
-            <Link href="/blog">
+            <Link href="/">
               <a className="flex w-auto pb-4">Blog</a>
             </Link>
           </li>
@@ -85,7 +85,7 @@ export default function MobileMenu() {
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
             style={{ transitionDelay: '275ms' }}
           >
-            <Link href="/snippets">
+            <Link href="/">
               <a className="flex w-auto pb-4">Snippets</a>
             </Link>
           </li>
@@ -93,7 +93,7 @@ export default function MobileMenu() {
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
             style={{ transitionDelay: '300ms' }}
           >
-            <Link href="/newsletter">
+            <Link href="/">
               <a className="flex w-auto pb-4">Newsletter</a>
             </Link>
           </li>
@@ -101,7 +101,7 @@ export default function MobileMenu() {
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
             style={{ transitionDelay: '325ms' }}
           >
-            <Link href="/tweets">
+            <Link href="/">
               <a className="flex w-auto pb-4">Tweets</a>
             </Link>
           </li>
@@ -109,7 +109,7 @@ export default function MobileMenu() {
             className="border-b border-gray-300 dark:border-gray-700 text-gray-900 dark:text-gray-100 text-sm font-semibold"
             style={{ transitionDelay: '350ms' }}
           >
-            <Link href="/uses">
+            <Link href="/">
               <a className="flex w-auto pb-4">Uses</a>
             </Link>
           </li>
