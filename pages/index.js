376fc61c4126dde5bb0024628ccb2ebd85e16da4
Ram Murmu <runashInc@outlook.com>
2022-05-16 12:31:50 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-import Container from '../components/Container';
+
 
 export default function Home() {
   return (
