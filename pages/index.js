caff38f4cf54ed93568c93bb56ab6587c867cd33
Ram Murmu <runashInc@outlook.com>
2022-05-16 12:02:42 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-
+import cn from 'classnames';
+import useDelayedRender from 'use-delayed-render';
+import { useState, useEffect } from 'react';
+import styles from 'styles/mobile-menu.module.css';
 
 export default function Home() {
   return (
