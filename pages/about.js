3a4ecd64772f57393ed9076c398d72c2bf6edb87
Ram Murmu <runashInc@outlook.com>
2022-05-17 22:22:05 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -7,9 +7,18 @@ export default function About() {
       <main className={styles.main}>
         <h1>About RunAsh</h1>
         <p className={styles.card}>👏 Welcome to RunAsh family!</p>
-        <p className={styles.grid}>🎤Introduction<br />
-        RunAsh Discipline and Culture<br />
-        Discipline and learning is the backbone of the development of our startup journey and runash culture is our behavior</p>
+        <div className={styles.grid}>
+         <a href="https://" className={styles.card}>
+         <h2>🎤Introduction</h2>
+         <p>RunAsh Discipline and Culture<br />
+         Discipline and learning is the backbone of the development of our startup journey and runash culture is our behavior</p>
+        </a>
+        
+        <a href="https://" className={styles.card}>
+        <h2>Oneliner</h2>
+        <p>RunAsh is an live streaming hybrid marketplace for retailing</p>
+        </a>
+
         <p className={styles.description}>
           <Link href="/">
             <a>&larr; Go Back</a>
