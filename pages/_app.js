734e3027097eebe8005b7571eef226d7dcb76c19
Ram Murmu <runashInc@outlook.com>
2022-05-18 11:15:05 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
    <Layout>
      <Component {...pageProps} />
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
     
 }
 
-export default MyApp
+
 
 
 
