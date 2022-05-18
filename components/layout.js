a5a446a6d0b4ae320a87bb1bc549201f4292baad
Ram Murmu <runashInc@outlook.com>
2022-05-18 11:47:17 +0530

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,16 @@
+import Head from 'next/head's
+import styles from '/layout.module.css
 import Navbar from './navbar'
 import Footer from './footer'
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+      <title>Layout Example</title>
       <Navbar />
-      <main>{children}</main>
+      <main className={styles.main}>{children}</main>
+      </Head>
       <Footer />
     </>
   )
