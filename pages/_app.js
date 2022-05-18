a9c8cd89c15deaf74463b8bef06dd01fb0826dab
Ram Murmu <runashInc@outlook.com>
2022-05-18 11:17:34 +0530

    Create _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
-import '../styles/globals.css'
 import Layout from '../components/layout'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-   <Layout>
-     <Component {...pageProps} />
-   </Layout>
-)  
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 
