aadcc8ea6a75b840f37a126edcb77550602e0743
Ram Murmu <runashInc@outlook.com>
2022-05-18 14:05:50 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const withMDX = require('@next/mdx')({
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
-     // If you use `MDXProvider`, uncomment the following line.
-     // providerImportSource: "@mdx-js/react",
+    // If you use `MDXProvider`, uncomment the following line.
+   // providerImportSource: "@mdx-js/react",
   },
 })
 module.exports = withMDX({
