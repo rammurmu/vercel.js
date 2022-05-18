f0c23c7839ed9762fea451cb2186b825475f6dd9
Ram Murmu <runashInc@outlook.com>
2022-05-18 16:53:28 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,37 +18,37 @@ export default function Home() {
            <a>About</a>
          </Link>
         </nav>
-        >
+        
         <nav>
          <Link href="/features">
            <a>Features</a>
          </Link>
         </nav>
-        >
+        
         <nav>
          <Link href="/solution">
            <a>Solution</a>
          </Link>
         </nav>
-        >
+        
         <nav>
          <Link href="/support">
            <a>Support</a>
          </Link>
         </nav>
-        >
+        
         <nav>
          <Link href="/legal">
            <a>Legal</a>
          </Link>
         </nav>
-        >
+        
         <nav>
          <Link href="/pricing">
            <a>Pricing</a>
          </Link>
         </nav>
-        >
+        
         <nav>
          <Link href="/resources">
            <a>Resources</a>
