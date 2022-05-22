4b8c5cfc3a37cc34c424928402574d142ab4cd2b
Ram Murmu <runashInc@outlook.com>
2022-05-22 22:21:08 +0530

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -6,22 +6,21 @@ export default function Contact() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>Contact Page</h1>
-    <p className={styles.grid}>👏Welcome to RunAsh</p>
+    <p className={styles.card}>👏Welcome to RunAsh</p>
     <div className={styles.grid}>
     <a href="https://" className={styles.card}>
-        <h1>CONTACTING SUPPORT</h1>
+        <h2>CONTACTING SUPPORT</h2>
         <p>ELSEWHERE ON THE INTERNET<br />Before contacting us , please check the frequently asked questions
 Here are contact emails for different part of runash<br />
 Admin to direct write:admin@runash.in<br />
 Sales ecutive:sales@runash.in<br />
-Support executive:Support@runash.in</p>
-
-<p>Headquarters
+Support executive:Support@runash.in<br />
+Headquarters
 RunAsh Digital Innovation Technologies Private Limited{' '}
 Bokaro Industrial Area BIADA Street Jharkhand India 827014{' '}
 PHONE NO 06542281002{' '}
 EMAIL ID admin@runash.in</p>
-
+</a>
         <p className={styles.description}>
           <Link href="/">
             <a>&larr; Go Back</a>
