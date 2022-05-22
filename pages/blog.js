d5e3f29215c1426f16737f7b91f9fd010335e909
Ram Murmu <runashInc@outlook.com>
2022-05-22 21:06:03 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,26 @@
-import Link from 'next/link';
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-<h1 className="title">
-  Read{' '}
-  <Link href="/posts/first-post">
-    <a>this page!</a>
-  </Link>
-</h1>
+export default function Blog() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Blog Page</h1>
+        <p className={styles.description}>
+          <Link href="/">
+        <h1 className="title">
+        Read{' '}
+        <Link href="/posts/first-post">
+         <a>this page!</a>
+        </Link>
+        </h1>
+
+            <a>&larr; Go Back</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
+
+
