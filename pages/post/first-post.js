ff04ceac3a855b7e68cc4f8488064d59b4f108ca
Ram Murmu <runashInc@outlook.com>
2022-05-22 21:35:02 +0530

    Update first-post.js

--- pages/post/first-post.js
+++ pages/post/first-post.js
@@ -5,7 +5,7 @@ export default function FirstPost() {
     <>
       <h1>First Post</h1>
       <h2>
-        <Link href="/first-post">
+        <Link href="">
           <a>Back to home</a>
         </Link>
       </h2>
