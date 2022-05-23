d7bbb9eee0347fdb017499c85786fd51fb1c170f
Ram Murmu <runashInc@outlook.com>
2022-05-23 10:59:48 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,20 +1,20 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-export default function Blog() {
+export default function FirstPost() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Blog Page</h1>
+        <h1>First Post</h1>
         <p className={styles.description}>
         <h1 className="title">
         Read{' '}
-        <Link href="/post">
+        <Link href="/posts/first-post">
           <a>this page!</a>
        </Link>
        </h1>
 
-          <Link href="/">
+          <Link href="/blog">
             <a>&larr; Go Back</a>
           </Link>
         </p>
