237ddf79f18bb472293db9985f4bb46a58ae505a
Ram Murmu <runashInc@outlook.com>
2022-05-23 10:55:39 +0530

    Update and rename pages/post/post.js to pages/posts/post.js

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 
-export default function Post() {
+export default function FirstPost() {
   return (
     <>
-      <h1>Post</h1>
+      <h1>First Post</h1>
       <h2>
-        <Link href="">
+        <Link href="index">
           <a>Back to home</a>
         </Link>
       </h2>
