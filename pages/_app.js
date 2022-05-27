798bfd0a58bf3206281c429b5ec4c920b841a39e
Ram Murmu <runashInc@outlook.com>
2022-05-27 21:47:36 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-    
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </Chakra provider>
+  )  
 }
 
 export default MyApp
-
 
 
 
