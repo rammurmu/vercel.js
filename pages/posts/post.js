2c48ee109795b261462e6fe6924819ab5b27f069
Ram Murmu <runashInc@outlook.com>
2022-05-27 09:11:58 +0530

    Update post.js

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 import Link from 'next/link';
-import Layout from '../../components/layout'
+
 
 export default function FirstPost() {
   return (
     <>
-      <Layout>
       <Head>
         <title>Blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,7 +15,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-      </Layout>
     </>
   );
 }
