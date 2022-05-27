e8db05e0bb3ec10487b2cf449ea6392b1bc0e2d4
Ram Murmu <runashInc@outlook.com>
2022-05-27 19:02:11 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,48 @@
 import styles from '../styles/Header.module.css'
+import type { NextPage } from "next"
 import Image from 'next/image'
 import Link from 'next/link'
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
-// Header component using <SignedIn> & <SignedOut>.
-//
-// The SignedIn and SignedOut components are used to control rendering depending
-// on whether or not a visitor is signed in.
-//
-// https://docs.clerk.dev/frontend/react/signedin-and-signedout
-const Header = () => (
+const Header:NextPage = () => (
+  const { openSignIn } = useClerk();
+
+   return (
   <header className={styles.header}>
+   <div className={styles.headerItem}>
     <div className={styles.left}>
       <Link href="/">
-        <a className={styles.logo}>
+        <a className={styles.homeButton}>RunAsh</a>
           <Image src="/logo.svg" width="32" height="32" alt="Logo" />
           <span className={styles.appName}>RunAsh</span>
         </a>
       </Link>
     </div>
+    <SignedIn>
+    <div className={styles.headerItem}>
     <div className={styles.right}>
+     <UserButton userProfileURL="/user" afterSignOutAll="/" />
+    </div>
+    </SignedIn>
       <SignedOut>
+        <div className={styles.headerItem}>
+          <UserButton userProfileURL="/user" afterSignOutAll="/" />
         <Link href="/sign-in">Sign in</Link>
       </SignedOut>
       <SignedIn>
         <UserButton userProfileURL="/user" afterSignOutAll="/" />
+      </div>
       </SignedIn>
+      </Signed out>
+         <div className={styles.headerItem}>
+           <a onClick={() => openSignIn()} className={styles.signInButton}>
+             Sign in
+           </a>
     </div>
+   <Signed out>
   </header>
 )
 
 export default Header
+
+      
