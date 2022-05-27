ea5149f875aaa938597ee988e4d7b3e433b2ef32
Ram Murmu <runashInc@outlook.com>
2022-05-27 11:43:45 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -6,6 +6,11 @@ export default function Buyer() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>Buyer Page</h1>
+          <h2>LIVE ON DEAMAND</h2>
+          <p>Whatching some live streaming,you can watch some live stream to receive notifications for new live streaming and updats.when the buyer updates the live streaming their organic products on daily basis, you'll see the changes in your personal dashboard.</p>
+          <h2>WATCH LIVE STREAMING</h2>
+          <p>Click watch at the top of a video to watch it.</p>
+
         <p className={styles.description}>
           <Link href="/">
             <a>&larr; Go Back</a>
