4260dbb662d1f5f88cb8fb600ec31b915396b98c
Ram Murmu <runashInc@outlook.com>
2022-05-30 16:56:25 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -72,11 +72,77 @@ export default function Home() {
           </ul>
          </nav>
           
-    <h5 className={styles.title}>
-        <a href="https://runash.in">Next-gen technology.<br />experience. </a>
-    </h5>
 
-    <div className={styles.description}>{' '}<code className={styles.code}>A live streaming hybrid marketplace for Sell✓ & Buy✓</code></div>
+const Header = () => (
+  <header>
+    <Link href="/">
+      <a className="logo" href="/">
+        <Logo />
+      </a>
+    </Link>
+
+
+    <style jsx>{`
+      header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+
+      nav {
+        padding: 10px 0 10px 5px;
+      }
+
+      nav a {
+        padding: 6px 10px;
+        display: inline-block;
+        text-decoration: none;
+        margin-right: 15px;
+      }
+
+      nav a.src {
+        font-size: 13px;
+        color: #333;
+      }
+
+      nav a.follow {
+        font-weight: bold;
+        color: #fff;
+        background: #000;
+        font-size: 13px;
+      }
+
+      a.logo {
+        text-decoration: none;
+        color: #000;
+        display: inline-flex;
+        transition: 150ms background-color ease;
+        padding: 5px;
+      }
+
+      @media (any-hover: hover) {
+        a.logo:hover {
+          background-color: var(--link-highlight);
+        }
+
+        a.logo:active {
+          background-color: #fff445;
+        }
+      }
+
+      @media (min-width: 500px) {
+        header {
+          max-width: 42rem;
+          margin: auto;
+          padding: 10px 0;
+        }
+
+        nav {
+          padding: 0;
+        }
+      }
+    `}</style>
+  
     
 
     <h5 className={styles.description}>
