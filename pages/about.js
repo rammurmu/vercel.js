c9a206ffa67fd7f15b28e0d3ebaff59ee3b17769
Ram Murmu <runashInc@outlook.com>
2022-06-06 16:40:07 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -6,24 +6,25 @@ export default function About() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>About RunAsh</h1>
-        <p className={styles.card}>👏 Welcome to RunAsh family!</p>
+        <code className={styles.inlinecode}>👏 Welcome to RunAsh family!</p>
+        <hr className={styles.hr}>
         <div className={styles.grid}>
-         <a href="https://" className={styles.card}>
+         <a href="https://" className={styles.cardContent}>
          <h2>Oneliner</h2>
          <p>RunAsh is a live streaming hybrid marketplace for retailing,where retailer face to face demonstrate, presentation their products with buyers </p>
         </a>
 
-        <a href="https://" className={styles.card}>
+        <a href="https://" className={styles.cardContent}>
          <h2>Discipline and culture</h2>
          <p>Discipline and learning is the backbone of the development of startup journey & culture is our behavior</p>
         </a>
 
-        <a href="https://" className={styles.card}>
+        <a href="https://" className={styles.cardContent}>
          <h2>What we do</h2>
          <p>We created new model of marketplace where retailers face to face demonstrate & presentation their products with buyers</p>
         </a>
 
-        <a href="https://" className={styles.card}>
+        <a href="https://" className={styles.cardContent}>
          <h2>What we going to make</h2>
          <p>We are commite to building live streaming platform and new ways to use physical reality experience</p>
         </a>
