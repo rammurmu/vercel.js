d8bb54f71d2cdec7eb0b1fdd2ee9bddda6c08d7e
Ram Murmu <runashInc@outlook.com>
2022-06-09 00:13:17 +0530

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -6,22 +6,27 @@ export default function Contact() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>Contact Page</h1>
-    <p className={styles.card}>👏Welcome to RunAsh</p>
+    <p className={styles.card}>👏Welcome to RunAsh.live</p>
     <div className={styles.grid}>
-    <a href="https://" className={styles.card}>
+    <div className={styles.description}>
         <h2>CONTACTING SUPPORT</h2>
-        <p>ELSEWHERE ON THE INTERNET Before contacting us , please check the frequently asked questions
-Here are contact emails for different part of runash
-Admin to direct write:admin@runash.in
-Sales ecutive:sales@runash.in
-Support executive:Support@runash.in
-Headquarters
-RunAsh Digital Innovation Technologies Private Limited
-Bokaro Industrial Area BIADA Street Jharkhand India 827014
-PHONE NO 06542281002
-EMAIL ID admin@runash.in</p>
-</a>
-</div>
+        <p>ELSEWHERE ON THE INTERNET <br />Before contacting us , please check the frequently asked questions Here are contact emails for different part of runash<br />
+        <>
+        <ul>
+        <ol>Admin to direct write:admin@runash.in</ol>
+        <ol>Sales ecutive:sales@runash.in</ol>
+        <ol>Support executive:Support@runash.in</ol>
+        </ul>
+        </>
+        <>
+        <p>Headquarters</p>
+        <p>RunAsh Digital Innovation Technologies Private Limited<br />
+        Bokaro Industrial Area BIADA Street Jharkhand India 827014<br />
+        PHONE NO 06542281002<br />
+        EMAIL ID admin@runash.in</p>
+       </>
+      </div>
+     </div>
         <p className={styles.description}>
           <Link href="/">
             <a>&larr; Go Back</a>
