63a5c8869a3e77ba8d617ab4eab8ffa0fa35926a
Ram Murmu <runashInc@outlook.com>
2022-06-09 22:40:01 +0530

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -5,7 +5,7 @@ export default function FAQ() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Frequently Asked Questions</h1>
+        <h1>FAQ</h1>
        
         <p className={styles.description}>
           <Link href="/">
