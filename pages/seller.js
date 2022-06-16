907fcdb58aed730f53f7a15c80b17c2595226f53
Ram Murmu <runashInc@outlook.com>
2022-06-16 06:58:41 +0530

    Update seller.js

--- pages/seller.js
+++ pages/seller.js
@@ -6,6 +6,12 @@ export default function Seller() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>Seller</h1>
+        <Container>
+  <FeedbackInput />
+
+  <Spacer y={8} />
+</Container>
+
         <p className={styles.description}>
           <Link href="/">
             <a>&larr; Go Back</a>
