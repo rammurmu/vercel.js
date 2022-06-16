37192742079257921a719bd66acdd9e8ebb01572
Ram Murmu <runashInc@outlook.com>
2022-06-16 06:49:45 +0530

    Update buyer.js

--- pages/buyer.js
+++ pages/buyer.js
@@ -38,6 +38,13 @@ export default function Buyer() {
           </a>
           </div>
 
+ <Video
+   src={
+   'https://assets.vercel.com/video/upload/v1542426673/front/design/examples/live-support.mp4'
+}
+  width={2000 / 3.5}
+  height={1300 / 3.5}
+/>
   
 
         <p className={styles.description}>
