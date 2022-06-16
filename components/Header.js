e34c9f275567f37d7aef739459fe7013d0ad33a5
Ram Murmu <runashInc@outlook.com>
2022-06-16 15:22:48 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,48 +1,35 @@
-import styles from '../styles/Header.module.css'
-import type { NextPage } from "next"
-import Image from 'next/image'
-import Link from 'next/link'
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import styles from "../styles/Header.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
-const Header:NextPage = () => (
-  const { openSignIn } = useClerk();
-
-   return (
+// Header component using <SignedIn> & <SignedOut>.
+//
+// The SignedIn and SignedOut components are used to control rendering depending
+// on whether or not a visitor is signed in.
+//
+// https://docs.clerk.dev/frontend/react/signedin-and-signedout
+const Header = () => (
   <header className={styles.header}>
-   <div className={styles.headerItem}>
     <div className={styles.left}>
       <Link href="/">
-        <a className={styles.homeButton}>RunAsh</a>
+        <a className={styles.logo}>
           <Image src="/logo.svg" width="32" height="32" alt="Logo" />
-          <span className={styles.appName}>RunAsh</span>
+          <span className={styles.appName}>Your application</span>
         </a>
       </Link>
     </div>
-    <SignedIn>
-    <div className={styles.headerItem}>
     <div className={styles.right}>
-     <UserButton userProfileURL="/user" afterSignOutAll="/" />
-    </div>
-    </SignedIn>
       <SignedOut>
-        <div className={styles.headerItem}>
-          <UserButton userProfileURL="/user" afterSignOutAll="/" />
         <Link href="/sign-in">Sign in</Link>
       </SignedOut>
       <SignedIn>
-        <UserButton userProfileURL="/user" afterSignOutAll="/" />
-      </div>
+        <UserButton userProfileURL="/user" afterSignOutAll="/" afterSignOutOneUrl="/" />
       </SignedIn>
-      </Signed out>
-         <div className={styles.headerItem}>
-           <a onClick={() => openSignIn()} className={styles.signInButton}>
-             Sign in
-           </a>
     </div>
-   <Signed out>
   </header>
-)
+);
 
-export default Header
+export default Header;
 
       
