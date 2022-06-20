992415d9adbedf6e10be3f8cadec36e1a4345658
Ram Murmu <runashInc@outlook.com>
2022-06-20 17:46:09 +0530

    Create app layout.js

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
