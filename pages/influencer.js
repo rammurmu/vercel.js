ebdbf24187be9e4d706667541c136d863567ca72
Ram Murmu <runashInc@outlook.com>
2022-06-20 19:08:57 +0530

    Rename

--- pages/influencer.js
+++ pages/influencer.js
@@ -1,11 +1,11 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-export default function Deshboard() {
+export default function Influencer() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Welcome to Dashboard!</h1>
+        <h1>Welcome to Influencer page!</h1>
         <p className={styles.description}>
           <Link href="/">
             <a>&larr; Go Back</a>
