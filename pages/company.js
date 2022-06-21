a49e68ab1f718b71548e1aa097c119a1ab2481be
Ram Murmu <runashInc@outlook.com>
2022-06-21 23:23:30 +0530

    Update company.js

--- pages/company.js
+++ pages/company.js
@@ -1,11 +1,11 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-export default function Company() {
+export default function Mobile() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Company Page</h1>
+        <h1>RunAsh Mobile</h1>
         <p className={styles.description}>
           <Link href="/">
             <a>&larr; Go Back</a>
