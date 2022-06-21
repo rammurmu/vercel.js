ce56fa410f820bd23566b3604fae35206a9521b6
Ram Murmu <runashInc@outlook.com>
2022-06-21 06:39:28 +0530

    Update index.js

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -1,12 +1,30 @@
 import ClientComponent from "./ClientComponent.js";
 import ServerComponent from "./ServerComponent.js";
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function ServerComponent() {
   return (
     <>
       <ClientComponent>
+
+      <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Welcome to back Ram!</h1>
+        <p className={styles.description}>
+          <Link href="/">
+            <a>&larr; Go Back</a>
+          </Link>
+        </p>
+      </main>
+    </div>
+
         <ServerComponent />
       </ClientComponent>
     </>
   );
 }
+
+
+
+  
