ae152c0314e136d05e29c9de5f4e0098ac12b4e0
Ram Murmu <runashInc@outlook.com>
2022-06-21 23:26:09 +0530

    Update and rename features.js to works.js

--- pages/works.js
+++ pages/works.js
@@ -1,11 +1,11 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-export default function Features() {
+export default function Works() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Features Page</h1>
+        <h1>How it works?</h1>
         <p className={styles.description}>
           <Link href="/">
             <a>&larr; Go Back</a>
