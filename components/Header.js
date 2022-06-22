be24048517361f5f849a3de82b60f61af1eaddbd
Ram Murmu <runashInc@outlook.com>
2022-06-22 16:06:01 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import styles from "../styles/Header.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "nextjs";
 
 // Header component using <SignedIn> & <SignedOut>.
 //
@@ -14,8 +14,8 @@ const Header = () => (
     <div className={styles.left}>
       <Link href="/">
         <a className={styles.logo}>
-          <Image src="/logo.svg" width="32" height="32" alt="Logo" />
-          <span className={styles.appName}>Your application</span>
+          <Image src="/vercel.svg" width="32" height="32" alt="Logo" />
+          <span className={styles.appName}>RunAsh.live</span>
         </a>
       </Link>
     </div>
