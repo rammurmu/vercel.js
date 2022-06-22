5a64ea282836bec9745da3bc6d1e1b58eb144be8
Ram Murmu <runashInc@outlook.com>
2022-06-22 13:55:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         
        <div className={styles.description}>{' '}<code className={styles.code}>👏 Welcome to RunAsh.Live</code></div>
       
-        <div className={styles.grid}>
+        <div className={styles.menu}>
 
         <nav>
           <div className={styles.menu}>
