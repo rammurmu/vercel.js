97296af2df3f4a14acf715554c921476ec76c850
Ram Murmu <runashInc@outlook.com>
2022-06-22 11:26:47 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -90,7 +90,7 @@ export default function Home() {
         
          
         <h1 className={styles.title}>
-         Live <a href="https://runash.in">retail</a> streaming< br /> <a href="https://runash.live">hybrid platform</a>
+         Live retail streaming< br /> <a href="https://runash.live">▶️hybrid</a> platform
         </h1>
         <p className={styles.description}>
           Where the retailers build own <br />made live streaming store</p>
@@ -99,7 +99,7 @@ export default function Home() {
         <div className={styles.description}>{' '}<code className={styles.code}>📺Go live</code></div>
         </a>
         <>
-         <p><a href="https://" title="title"><code classNa me={styles.code}>Geting live</code></a></p>
+         <p className={styles.description}><a href="https://" title="title"><code classNa me={styles.code}>Geting live</code></a></p>
         </>
     </Head>
 
