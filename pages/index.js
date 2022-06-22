d498f8f5dced165432428c0c056593faf1fb0ba7
Ram Murmu <runashInc@outlook.com>
2022-06-22 23:51:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { MDXProvider } from '@mdx-js/react'
 import Button from '../components/Button'
-
+import styles from '../styles/main.css'
 export default function Home() {
   return (
     <div className={styles.container}>
