21680fa1a78f0b467848b63592e0583f8a85bcdd
Ram Murmu <runashInc@outlook.com>
2022-06-23 21:58:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { MDXProvider } from '@mdx-js/react'
 import Button from '../components/Button'
-import Navbar from './components/navbar'
+
 
 
 
