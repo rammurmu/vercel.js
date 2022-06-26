17fda8a94fa2f7f83967f1cbcfece3b5b1a999a8
Ram Murmu <runashInc@outlook.com>
2022-06-26 15:36:56 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,7 @@ export default function Home() {
 
        <div className={styles.logo}>
        <a
-          href="https://runash.in"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         <strong>RunAsh</strong>{' '}
+          href="https://"
           <span className={styles.logo}>
             <Image src="vercel.svg" alt="logo" width={150} height={50} />
           </span>
